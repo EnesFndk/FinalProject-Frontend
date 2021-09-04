3bea189e341f5d3c221f5f9f895a17246bb97f9f
Enes Fındık <enesfindik.34@gmail.com>
2021-09-04 14:56:36 +0300

    18 2. click, currentcategoryclass

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './components/category/category.component';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+//routes [] içine ne yazarsak app.component.html içindeki router-outlet içinde değişiklik gösterecektir.
+//path = ana sayfada birşey verilmez ise ne olsun bizde hiç bir şey yazılmazsa product component olsun diyoruz.
+//pathMatch:"full" = bizim ana sayfamızla eşit olacak sonradan eklenecek şeyler için full dediğimiz için hata veriyormuş önceki sürümlerde bu sürümde vermiyor istersen silebilirsin.
+//path:"products = yazarsa productComponent'i aç
+//Özetle http://localhost:4200/products yazarsak bilgileri bize gösteriyor.
+const routes: Routes = [
+  {path:"", pathMatch:"full", component:ProductComponent},
+  {path:"products" , component:ProductComponent},
+  {path:"categories" , component:CategoryComponent}
+];
+
 
 @NgModule({
+  //bizim için root kombinasyonunu yapıyor zaten uygulamamız için routları devreye alıyor.
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -9,8 +9,11 @@ import { CategoryService } from 'src/app/services/category.service';
 })
 //Javascript ve typescript'de constructor'da bir değişken tanımladığımızda onu sanki public'miş gibi yapar.
 //Dışardan category Component'e erişen birinin service'lerede ulaşmasını istemediğimiz için private yapıyoruz.
+//currentCategory oluşturduk fakat onu initializer etmemiz gerekiyor. 3 şekilde yapabiliriz. bir currentCategory : Category={categoryId:0, categoryName:""}; böyle fake bir data veriyoruz.  2.ise tsconfig.json da strict altına "strictPropertyInitialization": false, yazmamız yeterli. ondan sonra currentCategory : Category; yazsakda hata vermez. 3. de class oluşturup newlemek en mantıklısı 2. yöntem.
 export class CategoryComponent implements OnInit {
   categories: Category[] = [];
+  currentCategory : Category;
+
   constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
@@ -21,6 +24,23 @@ export class CategoryComponent implements OnInit {
     this.categoryService.getCategries().subscribe(response => {
       this.categories = response.data
     })
+  }
+  setCurrentCategory(category:Category) {
+  this.currentCategory = category;
+  }
+
+  //burda hangi kategoriyi seçtiysek onun görseli mavi olsun istiyoruz. aynı zamanda category.component.html'de de değişiklikler yaptık.
+  //burda yaptığımız şey "dönerken ki kategoryi benim currentCategory'ime eşit ise onun currentCategoryClass'ını değiştir."
+  //Özet mantık şu ben category'leri dönerken currentCategory yazdık ya hani hangi categoryName'e tıkladığımızda onu seçtiniz diyordu ona eşit olursa onun css'ini o zaman aktif edicem
+  getCurrentCategoryClass(category:Category) {
+    if(category ==this.currentCategory) {
+      //eşitse mavi olacak.
+      return "list-group-item active"
+    }
+    else{
+      //değilse normal kalacak.
+      return "list-group-item"
+    }
   }
 
 }
